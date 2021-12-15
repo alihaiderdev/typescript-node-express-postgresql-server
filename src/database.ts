a780258dbbe72cad419543799262a6dd4d522e4d
alihaiderdev <alihaiderdev>
2021-12-15 15:54:05 +0500

    removing .env file from .gitignore

--- src/database.ts
+++ src/database.ts
@@ -8,3 +8,13 @@ export const pool = new Pool({
 	host: `${process.env.DATABASE_HOST}`,
 	port: 5432,
 });
+
+// links for amazon ecommerce
+// Link
+// https://github.com/amzn/selling-partner-api-docs
+
+// Developer Guide
+// https://github.com/amzn/selling-partner-api-docs/blob/main/guides/en-US/developer-guide/SellingPartnerApiDeveloperGuide.md
+
+// References (Main API's)
+// https://github.com/amzn/selling-partner-api-docs/tree/main/references
